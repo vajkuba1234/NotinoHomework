df1a94bec4fc52e02ed55107ad5753f3fd069197
Jakub Vajdík <jakub.vajdik@devlop.cz>
2022-08-05 20:41:08 +0200

    Serializers was added

---
+++ NotinoHomework/Serializers/JsonSerializer.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+
+namespace NotinoHomework.Serializers
+{
+    internal class JsonSerializer : ISerializer
+    {
+        T ISerializer.Deserialize<T>(string value)
+        {
+            var deserializationResult = JsonConvert.DeserializeObject<T>(value);
+
+            return deserializationResult;
+        }
+
+        string ISerializer.Serialize<T>(T value)
+        {
+            string serializationResult = JsonConvert.SerializeObject(value);
+
+            return serializationResult;
+        }
+    }
+}
---
+++ NotinoHomework/Serializers/XmlSerializer.cs
@@ -0,0 +1,15 @@
+﻿namespace NotinoHomework.Serializers
+{
+    internal class XmlSerializer : ISerializer
+    {
+        T ISerializer.Deserialize<T>(string value)
+        {
+            throw new NotImplementedException();
+        }
+
+        string ISerializer.Serialize<T>(T value)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
