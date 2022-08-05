e0e035169a3cfa19df2d00b2edb4edca9eb0361b
Jakub Vajdík <jakub.vajdik@devlop.cz>
2022-08-05 20:17:52 +0200

    Document class was added

---
+++ NotinoHomework/Common/Document.cs
@@ -0,0 +1,8 @@
+﻿namespace NotinoHomework.Common
+{
+    public class Document
+    {
+        public string Title { get; set; } = string.Empty;
+        public string Text { get; set; } = string.Empty;
+    }
+}
