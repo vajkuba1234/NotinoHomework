25d404e08738482124a654f1edb03e7f610b2733
Jakub Vajdík <jakub.vajdik@devlop.cz>
2022-08-11 16:17:26 +0200

    FileService was added

---
+++ NotinoHomework/Services/FileService.cs
@@ -0,0 +1,19 @@
+﻿namespace NotinoHomework.Api.Services
+{
+    public interface IFileService
+    {
+        Task<string> LoadAsync(Stream stream);
+    }
+
+    public class FileService : IFileService
+    {
+        async Task<string> IFileService.LoadAsync(Stream stream)
+        {
+            using var reader = new System.IO.StreamReader(stream);
+
+            string result = await reader.ReadToEndAsync();
+
+            return result;
+        }
+    }
+}
