f7e66a366eedc4dbc7c71498b15c2ec2069b9800
Jakub Vajdík <jakub.vajdik@devlop.cz>
2022-08-11 16:16:27 +0200

    Queries were added

---
+++ NotinoHomework/Queries/GetDocumentQuery.cs
@@ -0,0 +1,10 @@
+﻿using NotinoHomework.Common;
+
+namespace NotinoHomework.Api.Queries
+{
+    public class GetDocumentQuery : MediatR.IRequest<Document>
+    {
+        public string Title { get; set; } = "Get title";
+        public string Text { get; set; } = "Get text";
+    }
+}
---
+++ NotinoHomework/Queries/GetDocumentQueryHandler.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+using NotinoHomework.Common;
+
+namespace NotinoHomework.Api.Queries
+{
+    public class GetDocumentQueryHandler : MediatR.IRequestHandler<GetDocumentQuery, Document>
+    {
+        Task<Document> IRequestHandler<GetDocumentQuery, Document>.Handle(GetDocumentQuery request, CancellationToken cancellationToken)
+        {
+            var response = new Document
+            {
+                Title = request.Title,
+                Text = request.Text
+            };
+
+            return Task.FromResult(response);
+        }
+    }
+}
