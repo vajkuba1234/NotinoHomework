7612c4dd8971d93c42872edfdef68f22854fcad6
Jakub Vajdík <jakub.vajdik@devlop.cz>
2022-08-11 16:17:31 +0200

    Amendment

--- NotinoHomework/Program.cs
+++ NotinoHomework/Program.cs
@@ -1,13 +1,29 @@
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using NotinoHomework.Api.Commands;
+using NotinoHomework.Api.Common.ViewModels;
+using NotinoHomework.Api.Queries;
+using NotinoHomework.Api.Serializers;
+using NotinoHomework.Api.Serializers.Abstractions;
+using NotinoHomework.Api.Services;
+using System.Reflection;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddMediatR(Assembly.GetExecutingAssembly());
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+
+builder.Services.AddTransient<IFileService, FileService>();
+builder.Services.AddTransient<IByteSerializer, JsonByteSerializer>();
+builder.Services.AddTransient<IByteSerializer, XmlByteSerializer>();
+builder.Services.AddTransient<IStringSerializer, JsonStringSerializer>();
+builder.Services.AddTransient<IStringSerializer, XmlStringSerializer>();
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -16,28 +32,23 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
+app.MapGet("/homeworks/prefilled", async (ISender mediator, CancellationToken cancellationToken) =>
 {
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+    var result = await mediator.Send(new GetDocumentQuery(), cancellationToken);
 
-app.MapGet("/weatherforecast", () =>
+    return Results.Ok(result);
+});
+
+app.MapPost("/homeworks/convert", async (ISender mediator, [FromBody] ConvertDocumentRequestViewModel viewModel) =>
 {
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateTime.Now.AddDays(index),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
+    var result = await mediator.Send(new ConvertDocumentCommand { ConvertTo = viewModel.FileType, FormFile = viewModel.FormFile });
+
+    if (viewModel.Email is not null)
+    {
+        //_ = await mediator.Send(new SendEmailCommand { ... });
+    }
+
+    return Results.File(result.Content, "application/json", result.FileName);
+});
 
 app.Run();
-
-internal record WeatherForecast(DateTime Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
