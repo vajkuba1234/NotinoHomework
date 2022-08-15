03f10f25be63446c824bc003ae46c51b05f44db4
Jakub Vajdík <jakub.vajdik@devlop.cz>
2022-08-15 17:19:03 +0200

    Amendment

--- NotinoHomework/Serializers/XmlByteSerializer.cs
+++ NotinoHomework/Serializers/XmlByteSerializer.cs
@@ -5,9 +5,20 @@ namespace NotinoHomework.Api.Serializers
 {
     public class XmlByteSerializer : IByteSerializer
     {
+        private readonly XmlRootAttribute xmlRootAttribute;
+
+        public XmlByteSerializer() : this(GetDefaultXmlRootAttribute())
+        {
+        }
+
+        public XmlByteSerializer(XmlRootAttribute xmlRootAttribute)
+        {
+            this.xmlRootAttribute = xmlRootAttribute ?? throw new ArgumentNullException(nameof(xmlRootAttribute));
+        }
+
         public T? Deserialize<T>(byte[] value)
         {
-            var serializer = new XmlSerializer(typeof(T));
+            var serializer = new XmlSerializer(typeof(T), xmlRootAttribute);
 
             using var ms = new MemoryStream(value);
 
@@ -16,13 +27,18 @@ namespace NotinoHomework.Api.Serializers
 
         public byte[] Serialize<T>(T value)
         {
-            var serializer = new XmlSerializer(typeof(T));
+            var serializer = new XmlSerializer(typeof(T), xmlRootAttribute);
 
             using var ms = new MemoryStream();
 
             serializer.Serialize(ms, value);
 
             return ms.ToArray();
+        }
+
+        private static XmlRootAttribute GetDefaultXmlRootAttribute()
+        {
+            return new XmlRootAttribute { ElementName = "root", IsNullable = true };
         }
     }
 }
