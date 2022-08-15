82e6a7d99a6b2bdbf260b34fa2126c4c35495baa
Jakub Vajdík <jakub.vajdik@devlop.cz>
2022-08-15 17:21:50 +0200

    Send emial command was added

---
+++ NotinoHomework/Commands/SendEmailCommand.cs
@@ -0,0 +1,9 @@
+﻿namespace NotinoHomework.Api.Commands
+{
+    public class SendEmailCommand : MediatR.IRequest
+    {
+        public string From { get; set; } = string.Empty;
+        public string To { get; set; } = string.Empty;
+        public string Subject { get; set; } = string.Empty;
+    }
+}
---
+++ NotinoHomework/Commands/SendEmailCommandHandler.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+
+namespace NotinoHomework.Api.Commands
+{
+    public class SendEmailCommandHandler : MediatR.IRequestHandler<SendEmailCommand>
+    {
+        Task<Unit> IRequestHandler<SendEmailCommand, Unit>.Handle(SendEmailCommand request, CancellationToken cancellationToken)
+        {
+
+
+            throw new NotImplementedException();
+        }
+    }
+}
