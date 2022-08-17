311153ab685bafb86829339a768364c77ad68b93
Jakub Vajdík <jakub.vajdik@devlop.cz>
2022-08-17 17:02:07 +0200

    Services - abstractions were added

---
+++ NotinoHomework/Services/Abstractions/IEmailService.cs
@@ -0,0 +1,7 @@
+﻿namespace NotinoHomework.Api.Services.Abstractions
+{
+    public interface IEmailService
+    {
+        Task SendAsync(string from, string to, string subject, Stream content, string fileName, CancellationToken token = default);
+    }
+}
---
+++ NotinoHomework/Services/Abstractions/IFileService.cs
@@ -0,0 +1,9 @@
+﻿namespace NotinoHomework.Api.Services.Abstractions
+{
+    public interface IFileService
+    {
+        Task<string> ConvertAsync(Common.FileType convertFrom, Common.FileType convertTo, Stream stream, string fileName, CancellationToken token = default);
+        Task<Stream> LoadAsync(string filePath);
+        Task SaveAsync(string filePath, Stream stream, CancellationToken token = default);
+    }
+}
