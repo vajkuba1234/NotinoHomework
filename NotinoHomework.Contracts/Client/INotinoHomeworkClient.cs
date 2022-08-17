b024051ed7f595e7bc0a53c9b2810ee6d9c9aee0
Jakub Vajdík <jakub.vajdik@devlop.cz>
2022-08-18 00:00:46 +0200

    Client was added

---
+++ NotinoHomework.Contracts/Client/INotinoHomeworkClient.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NotinoHomework.Api.Common.ViewModels;
+
+namespace NotinoHomework.Contracts.Client
+{
+    public interface INotinoHomeworkClient
+    {
+        Task<IActionResult> ConvertFileAsync([FromForm] ConvertDocumentRequestViewModel viewModel, CancellationToken token);
+    }
+}
